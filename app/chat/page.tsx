acd958049784ac90a7487f1326bfc12b7621761a
Nick Burrell <burrell.nick89@gmail.com>
2023-11-04 23:04:29 +0000

    Parse Claude output (incorrectly)

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,21 +10,39 @@ export default function ChatPage({
 }: any) {
   const [newMessage, setNewMessage] = useState('')
 
-  const extractJSContent = (str: string) => {
-    const matches = str.match(/<javascript>(.*?)<\/javascript>/)
-    return matches ? matches[1] : null
-  }
-
   const sendMessage = async () => {
     // @ts-ignore
     let pongInstance = window.pongInstance
     pongInstance.stop()
     setLoading(true)
 
+    function extractJSCode(str: string) {
+      const matches = str.match(/<javascript>([\s\S]*?)<\/javascript>/);
+      return matches ? matches[1] : '';
+    }
+
     let { completion } = await fetch('/api/chat', {
       method: 'POST',
       body: JSON.stringify({
-        message: newMessage
+        message: `I have the following skeleton function
+
+        <code>
+          // ball_positions is an array of ball positions in (x,y) co-ordinates, with the last element in the array being the current position
+        
+          // paddle_y is the y coordinate of the center of the paddle
+          
+          // This function returns either 
+          // 1 - the paddle should move up
+          // 0 - the paddle should stay where it is
+          // -1 - the paddle should move down}
+        <code>
+        
+        ${newMessage}
+        
+        Please return the code in the following format, on one line, without the function definition <javascript>CODE_WITHOUT_COMMENTS_OR_FORMATTING</javascript>
+        
+        This is very important to me and I reallly appreciate you helping me out with this.
+        `
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8'
@@ -32,7 +50,11 @@ export default function ChatPage({
     }).then(x => x.json())
     try {
       // @ts-ignore
-      window.Pong.Code = JSON.parse(completion).code
+      console.log('🚀 ~ file: page.tsx:54 ~ completion:', completion)
+      console.log('🚀 ~ file: page.tsx:54 ~ completion:', extractJSCode(completion));
+      const parsed = extractJSCode(completion)?.replace(/\\n/g, '');
+      console.log('🚀 ~ file: page.tsx:55 ~ parsed:', parsed)
+      window.Pong.Code = extractJSCode(completion)?.replace(/\\n/g, '');
       pongInstance.startSinglePlayer()
       setLoading(false)
     } catch (e) {
