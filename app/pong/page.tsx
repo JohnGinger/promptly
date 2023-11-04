685f003447d2f653bcfff82492dfcd5f42be63e6
John Ginger <github@johnginger.co.uk>
2023-11-04 15:37:45 +0000

    Get pong working
    Co-authored-by: nbml <nbml@users.noreply.github.com>
    Co-authored-by: Florence Hinder <Florencehinder@users.noreply.github.com>

--- app/pong/page.tsx
+++ app/pong/page.tsx
@@ -11,7 +11,9 @@ export default function PongPage() {
       footprints: false,
       predictions: false
     })
-    pong.startSinglePlayer()
+    setTimeout(() => {
+      pong.startSinglePlayer()
+    }, 200)
   }
   return (
     <>
