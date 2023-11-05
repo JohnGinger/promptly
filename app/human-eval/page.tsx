2a6df31ffe51cf912d8b568208c24ea38e0d1336
John Ginger <github@johnginger.co.uk>
2023-11-05 09:28:55 +0000

    Formatting fix and add hint

--- app/human-eval/page.tsx
+++ app/human-eval/page.tsx
@@ -39,38 +39,38 @@ export default function HumanEvalPage() {
               try {
                 let func = new Function('list', parsed)
 
-// Your goal is to write a function that accepts a list of strings as a parameter, deletes the strings that have odd lengths from it, and returns the resulted list with a sorted order,
-// The list is always a list of strings and never an array of numbers, and it may contain duplicates.
+                // Your goal is to write a function that accepts a list of strings as a parameter, deletes the strings that have odd lengths from it, and returns the resulted list with a sorted order,
+                // The list is always a list of strings and never an array of numbers, and it may contain duplicates.
 
-// The order of the list should be ascending by length of each word, and you should return the list sorted by that rule.
+                // The order of the list should be ascending by length of each word, and you should return the list sorted by that rule.
 
-// If two words have the same length, sort the list alphabetically.
+                // If two words have the same length, sort the list alphabetically.
 
-// The function should return a list of strings in sorted order, using standard compare, NOT localCompare.  Return uppercase BEFORE lowercase, all other things being equal.
+                // The function should return a list of strings in sorted order, using standard compare, NOT localCompare.  Return uppercase BEFORE lowercase, all other things being equal.
 
-// You may assume that all words will have the same length.
+                // You may assume that all words will have the same length.
 
-// For example: sorted_list_sum(["aa", "a", "aaa"]) => ["aa"] sorted_list_sum(["ab", "a", "aaa", "cd"]) => ["ab", "cd"]
+                // For example: sorted_list_sum(["aa", "a", "aaa"]) => ["aa"] sorted_list_sum(["ab", "a", "aaa", "cd"]) => ["ab", "cd"]
 
-// THE FOLLOWING TWO EXAMPLES ARE VERY IMPORTANT, IT MUST WORK FOR BOTH
-// sorted_list_sum(['aaaa', 'bbbb', 'dd', 'cc']) => ['cc','dd','aaaa','bbbb']
-// sorted_list_sum(['AI', 'ai', 'au']) => ['AI','ai','au']
+                // THE FOLLOWING TWO EXAMPLES ARE VERY IMPORTANT, IT MUST WORK FOR BOTH
+                // sorted_list_sum(['aaaa', 'bbbb', 'dd', 'cc']) => ['cc','dd','aaaa','bbbb']
+                // sorted_list_sum(['AI', 'ai', 'au']) => ['AI','ai','au']
 
-// Implement the function in Javascript, bracketed in <javascript> tags. Only implement AND RETURN the function body. Pay very close attention to the next instruction: you MUST OMIT the function header, you must not return a variable.
+                // Implement the function in Javascript, bracketed in <javascript> tags. Only implement AND RETURN the function body. Pay very close attention to the next instruction: you MUST OMIT the function header, you must not return a variable.
 
-// For example:
-// BAD:
-// let sortedList = list => { ...the_code }
-// GOOD:
-// return the_code
+                // For example:
+                // BAD:
+                // let sortedList = list => { ...the_code }
+                // GOOD:
+                // return the_code
 
                 let responses = [
-                  func(["aa", "a", "aaa"]),
-                  func(["school", "AI", "asdf", "b"]),
-                  func(["d", "b", "c", "a"]),
-                  func(["d", "dcba", "abcd", "a"]),
-                  func(["AI", "ai", "au"]),
-                  func(["a", "b", "b", "c", "c", "a"]),
+                  func(['aa', 'a', 'aaa']),
+                  func(['school', 'AI', 'asdf', 'b']),
+                  func(['d', 'b', 'c', 'a']),
+                  func(['d', 'dcba', 'abcd', 'a']),
+                  func(['AI', 'ai', 'au']),
+                  func(['a', 'b', 'b', 'c', 'c', 'a']),
                   func(['aaaa', 'bbbb', 'dd', 'cc'])
                 ]
                 console.log('responses', responses)
@@ -99,22 +99,33 @@ export default function HumanEvalPage() {
                 </span>
                 <span className="text-black text-base font-normal font-sans leading-normal">
                   {' '}
-                  is to write a function that accepts a list of strings as a parameter, 
-    deletes the strings that have odd lengths from it,
-    and returns the resulted list with a sorted order,
-    </span>
-    <p>
-    The list is always a list of strings and never an array of numbers,
-    and it may contain duplicates. </p>
-    <p>The order of the list should be ascending by length of each word, and you 
-    should return the list sorted by that rule.</p>
-    <p>If two words have the same length, sort the list alphabetically.</p>
-    <p>The function should return a list of strings in sorted order.</p>
-    <p>You may assume that all words will have the same length.</p>
-    <p>For example:
-    sorted_list_sum([&quot;aa&quot;, &quot;a&quot;, &quot;aaa&quot;]) =&gt; [&quot;aa&quot;] 
-    sorted_list_sum([&quot;ab&quot;, &quot;a&quot;, &quot;aaa&quot;, &quot;cd&quot;]) =&gt; [&quot;ab&quot;, &quot;cd&quot;]</p>
- 
+                  is to write a function that accepts a list of strings as a
+                  parameter, deletes the strings that have odd lengths from it,
+                  and returns the resulted list with a sorted order,
+                </span>
+                <p>
+                  The list is always a list of strings and never an array of
+                  numbers, and it may contain duplicates.{' '}
+                </p>
+                <p>
+                  The order of the list should be ascending by length of each
+                  word, and you should return the list sorted by that rule.
+                </p>
+                <p>
+                  If two words have the same length, sort the list
+                  alphabetically.
+                </p>
+                <p>
+                  The function should return a list of strings in sorted order.
+                </p>
+                <p>You may assume that all words will have the same length.</p>
+                <p>
+                  For example: sorted_list_sum([&quot;aa&quot;, &quot;a&quot;,
+                  &quot;aaa&quot;]) =&gt; [&quot;aa&quot;]
+                  sorted_list_sum([&quot;ab&quot;, &quot;a&quot;,
+                  &quot;aaa&quot;, &quot;cd&quot;]) =&gt; [&quot;ab&quot;,
+                  &quot;cd&quot;]
+                </p>
               </div>
             }
           />
