f331858774fa9ca85fed0096350e0a0143325536
John Ginger <github@johnginger.co.uk>
2023-12-01 13:35:23 +0000

    Improve g analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-8121SQR4CB" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-8121SQR4CB');
+        `}
+      </Script>
 
       <body className={inter.className}>{children}</body>
     </html>
