85205c3c365d6c030652c469ced43cf9f9e224a5
John Ginger <github@johnginger.co.uk>
2023-12-01 13:45:20 +0000

    Embed directly

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,26 @@ export default function RootLayout({
           gtag('config', 'G-8121SQR4CB');
         `}
       </Script>
-      <Script src="https://static.hotjar.com/c/hotjar-3745393.js?sv=6" />
+      <Script id="hotjar">
+        {
+          `
+          ;(function (h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function () {
+                ;(h.hj.q = h.hj.q || []).push(arguments)
+              }
+            h._hjSettings = { hjid: 3745393, hjsv: 6 }
+            a = o.getElementsByTagName('head')[0]
+            r = o.createElement('script')
+            r.async = 1
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+            a.appendChild(r)
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+          `
+        }
+      </Script>
+
 
       <body className={inter.className}>{children}</body>
     </html>
