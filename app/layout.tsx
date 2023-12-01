f87e4094c2f014f7a5b6075f29d6acccc2e64817
John Ginger <github@johnginger.co.uk>
2023-12-01 13:24:05 +0000

    Try moving where things

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-8121SQR4CB" />
+
       <body className={inter.className}>{children}</body>
     </html>
   )
